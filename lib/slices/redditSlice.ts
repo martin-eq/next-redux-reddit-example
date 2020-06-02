81500c083d2f42929eae8ab82e1ca3fcbf57e268
Martin Quintana <quintanamartine@gmail.com>
2020-06-02 01:46:04 -0300

    Fix linting issues in redditSlice

--- lib/slices/redditSlice.ts
+++ lib/slices/redditSlice.ts
@@ -9,54 +9,59 @@ type RedditState = {
   currentPost?: Post
   loading: string
   error?: string
+  after?: string
 }
 type StateType = {
   reddit: RedditState
 }
 
-export const fetchPosts = createAsyncThunk(
-  'notes/fetchPosts',
-  async (params = {}, thunkAPI) => {
-    try {
-      const response = await ky.get(`${API_URL}/top.json`, {
-        searchParams: {
-          after: params.after,
-          limit: PAGE_LIMIT,
-        },
-      })
+type ParamsType = {
+  after: string
+}
 
-      return response.json()
-    } catch (error) {
-      console.error(error)
-      return thunkAPI.rejectWithValue({ error: error.message })
-    }
+export const fetchPosts = createAsyncThunk<
+  Promise<string>,
+  Nullable<ParamsType>
+>('notes/fetchPosts', async (params, thunkAPI) => {
+  try {
+    const response = await ky.get(`${API_URL}/top.json`, {
+      searchParams: {
+        after: params ? params.after : '',
+        limit: PAGE_LIMIT,
+      },
+    })
+
+    return response.json()
+  } catch (error) {
+    console.error(error)
+    return thunkAPI.rejectWithValue({ error: error.message })
   }
-)
+})
+
+const initialState: RedditState = {
+  posts: [],
+  loading: 'idle',
+}
 
 const redditSlice = createSlice({
   name: 'reddit',
-  initialState: {
-    posts: [],
-    currentPost: null,
-    error: null,
-    loading: 'idle',
-  },
+  initialState,
   reducers: {
     setCurrentPost: (state, action) => {
       state.currentPost = action.payload
     },
   },
   extraReducers: {
-    [fetchPosts.pending]: (state) => {
+    [fetchPosts.pending.toString()]: (state) => {
       state.posts = []
       state.loading = 'loading'
     },
-    [fetchPosts.fulfilled]: (state, { payload }) => {
+    [fetchPosts.fulfilled.toString()]: (state, { payload }) => {
       state.posts = payload.data.children.map((child) => child.data)
       state.after = payload.data.after
       state.loading = 'loaded'
     },
-    [fetchPosts.rejected]: (state, { payload }) => {
+    [fetchPosts.rejected.toString()]: (state, { payload }) => {
       state.loading = 'error'
       state.error = payload.error
     },
